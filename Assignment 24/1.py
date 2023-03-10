4ea5f6ceaadec873a3e9ef0f6d375a1493605027
mohamad.nematizadeh <89206040+Mohammadnematizade@users.noreply.github.com>
2023-03-10 04:11:10 +0330

    Add files via upload

---
+++ Assignment 24/1.py
@@ -0,0 +1,14 @@
+
+import moviepy.editor as mp
+import time
+
+start_time = time.time()
+
+for i in range(1,2):
+    video = mp.VideoFileClip(f"{i}.mp4")
+    audio = video.audio
+    audio.write_audiofile(f"{i}.mp3")
+
+end_time = time.time()
+
+print( end_time - start_time)
